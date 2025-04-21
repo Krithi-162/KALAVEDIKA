7e1871578076d36bfb6bf4a8ff20427d32c6a3a3
krithi <krithikulal17@gmail.com>
2025-04-21 15:42:59 +0530

    added bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,12 +10,12 @@ export default function Home() {
   }, []);
 
   return (
-<div className="relative min-h-screen w-full font-['Roboto'] flex flex-col overflow-hidden">
-  {/* Blurred + semi-transparent background image */}
-  <div className="absolute inset-0 bg-[url('/cultural-bg.jpeg')] bg-cover bg-center blur opacity-65 z-0"></div>
+<div className="relative min-h-screen w-full font-[' '] flex flex-col overflow-hidden">
+ 
+  <div className="absolute inset-0 bg-[url('/cultural-bg.jpg')] bg-cover bg-center"></div>
 
       {/* Header */}
-      <header className="bg-white shadow-md px-4 md:px-8 py-4 flex justify-between items-center bg-white/90 backdrop-blur-sm shadow-md px-4 md:px-8 py-4 flex justify-between items-center z-10">
+      <header className="bg-black shadow-md px-4 md:px-8 py-4 flex justify-between items-center bg-white/90 backdrop-blur-sm shadow-md px-4 md:px-8 py-4 flex justify-between items-center z-10">
         <img src="/ajiet-logo.svg" alt="AJIET Logo" className="h-13 w-auto" />
        <nav className="hidden md:flex space-x-6 md:space-x-8">
           <a href="#" className="text-gray-800 hover:text-gray-600 transition">
@@ -34,13 +34,21 @@ export default function Home() {
       <main className="flex flex-col items-center justify-between flex-1 text-center px-4 py-16">
         {/* Title */}
         <h1
-          className={`mt-33 text-3xl sm:text-4xl md:text-8xl text-white font-bold transition-opacity duration-1000 ease-in-out ${
+          style={{ color: "#930a00" }}
+          className={`mt-30 text-2xl sm:text-4xl md:text-8xl font-bold transition-opacity duration-1000 ease-in-out ${
           mounted ? "opacity-98" : "opacity-0"
-         }`}
+  }`}
 >
-              CULTURAL COMMITTEE
-        </h1>
-
+  CULTURAL
+</h1>
+<h1
+          style={{ color: "#930a00" }}
+          className={`mt-20 text-2xl sm:text-4xl md:text-8xl font-bold transition-opacity duration-1000 ease-in-out ${
+          mounted ? "opacity-98" : "opacity-0"
+  }`}
+>
+  COMMITTEE
+</h1>
         {/* Spacer to push buttons down */}
         <div className="flex flex-col items-center mt-auto mb-12">
           <div className="flex flex-col sm:flex-row gap-4 sm:gap-12">
